48e2063b58ff59aafbd318c752e43701b1f90728
REHAN JAMIL <44650690+rehanjamil10@users.noreply.github.com>
2020-10-31 10:12:31 +0530

    Add files via upload

---
+++ add_data_list.py
@@ -0,0 +1,13 @@
+# add data to a list
+fruits = ['orange','apple']
+fruits.append('mango')
+print(fruits)
+
+
+fruits=[]
+fruits.append('mango')
+fruits.append('orange')
+fruits.append('guava')
+fruits.append('apple')
+fruits.append('litchi')
+print(fruits)
